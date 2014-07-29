afe05441d2f2cb4552ac814ff4910b02e6350956
Jan Kaliszewski <zuo@kaliszewski.net>
2014-07-29 02:58:49 +0200

    Minor Sphinx config improvement

--- docs/conf.py
+++ docs/conf.py
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'unittest_expanderdoc'
+htmlhelp_basename = 'unittest_expander_doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
